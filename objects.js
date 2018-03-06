a699077accedcdb5404692b5d9c6d7be501b457b
guptaco@ymail.com <guptaco@ymail.com>
2018-03-06 00:52:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist = {
 };
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  var playlist = {artistName: 'songTitle'}
+  playlist[artistName] = songTitle
   return playlist
 }
